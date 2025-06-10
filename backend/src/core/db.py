807bf7096da42a49bf233c917c1c3241a0d36e94
zlatanovics1 <office@zlatanovics.com>
2025-06-10 11:30:52 +0200

    fix(be): avoiding idle supabase connections

--- backend/src/core/db.py
+++ backend/src/core/db.py
@@ -4,7 +4,7 @@ from typing import Generator
 from sqlmodel import Session
 
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, pool_size=10, max_overflow=2, pool_recycle=299, pool_pre_ping=True)
 
 def get_db() -> Generator[Session, None, None]:
     with Session(engine) as session:
